8e879d916a2cb98ff3efc56a6bd814a2919356ff
sohamsshah <sohamshah456@gmail.com>
2021-05-14 21:40:39 +0530

    fix category util function

--- client/src/utils/category/category.js
+++ client/src/utils/category/category.js
@@ -1,8 +1,3 @@
 export const searchCreator = (data, creator_id) => {
-    let res = (data.creators.filter((creator) => creator.creator_id === creator_id)) 
-    if(res.length === 0){
-        return (data.channels.filter((channel) => channel.creator_id === creator_id))
-        
-    }
-    return res
+    return data.creators.find((item) => item.creator_id === creator_id);
 }
