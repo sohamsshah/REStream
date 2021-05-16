870c15399638bfacb1e69022c2ce09081b26ec59
Soham Shah <>
2021-05-16 15:45:12 +0000

    configure userCredentials route

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const { routeNotFound } = require('./server/middlewares/route-not-found.middlewa
 const { addVideosToDB } = require('./server/models/video.model');
 const { addCreatorsToDB } = require('./server/models/creator.model');
 const { addCategoriesToDB } = require('./server/models/category.model');
+const { addUserCredToDB } = require('./server/models/userCredentials.model');
+
 const video = require('./server/routes/videos.route');
 const categories = require('./server/routes/categories.route');
 const creators = require('./server/routes/creators.route');
+const userCredential = require('./server/routes/userCredentials.route');
 
 // Data parsing
 app.use(express.json());
@@ -16,11 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 
 const { initializeDatabase } = require("./server/db/db.connect");
 initializeDatabase();
+// addUserCredToDB();
 // addCategoriesToDB();
 //addVideosToDB();
 //addCreatorsToDB();
 
 app.use('/watch', video);
+app.use('/auth', userCredential);
 app.use('/categories', categories);
 app.use('/creators', creators);
 
