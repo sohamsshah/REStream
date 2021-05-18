9828e69c5f28a9b28239c678a738a471842e2698
sohamsshah <sohamshah456@gmail.com>
2021-05-19 01:14:03 +0530

    update category

--- client/src/components/organisms/Category/Category.jsx
+++ client/src/components/organisms/Category/Category.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import {useParams} from "react-router-dom"
 import {data} from "./../../../data/data"
 import {searchCreator} from "./../../../utils/category/category"
@@ -9,10 +10,26 @@ import Typography from "./../../atoms/Typography/Typography"
 import "./Category.css"
 
 function Category() {
+    const [videos, setVideo] = useState()
     const { category } = useParams();
     const categoryVideos = data.videos.filter((item) => item.category_name === category);
     const {name, thumbnail, description, tags} = data.categories.filter((item) => item.category_name === category)[0];
-    console.log(categoryVideos);
+    console.log(videos);
+
+    useEffect(() => {
+        (async function() {
+            
+            try{
+            const response = await axios.get(`https://apirestream.sohamsshah.repl.co/categories/${category}`) ;
+            if(response.status === 200) {
+              setVideo(response.data.videos)}
+             }
+             catch(error){
+                 
+                 console.log(error.message)
+             }
+          })()
+    }, [])
     return (
         <div>        
         <CategoryDetails name={name} thumbnail={thumbnail} description={description} tags={tags}/>
--- client/src/utils/category/category.js
+++ client/src/utils/category/category.js
@@ -1,5 +1,3 @@
 export const searchCreator = (data, creator_id) => {
-    console.log(creator_id,data);
-    console.log(data.creators.filter((item) => item.creator_id === creator_id));
     return data.creators.filter((item) => item.creator_id === creator_id);
 }
