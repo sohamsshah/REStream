2ece7127a5f330a448fbac3893510b6708de9c3f
sohamsshah <sohamshah456@gmail.com>
2021-05-20 08:12:03 +0530

    complete category page

--- client/src/components/organisms/Category/Category.jsx
+++ client/src/components/organisms/Category/Category.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import {useParams} from "react-router-dom"
-import {data} from "./../../../data/data"
-import {searchCreator} from "./../../../utils/category/category"
+import { useParams } from "react-router-dom"
+import { data } from "./../../../data/data"
+import { searchCreator } from "./../../../utils/category/category"
 import Video from "./../../molecules/Video/Video"
 import VideoGroup from "./../VideoGroup/VideoGroup"
 import CategoryDetails from "./../CategoryDetails/CategoryDetails"
@@ -10,35 +10,42 @@ import Typography from "./../../atoms/Typography/Typography"
 import "./Category.css"
 
 function Category() {
-    const [videos, setVideo] = useState()
+    const [categoryVideos, setCategoryVideos] = useState("Loading...")
+    const [categoryDetails, setCategoryDetails] = useState("Loading...")
     const { category } = useParams();
-    const categoryVideos = data.videos.filter((item) => item.category_name === category);
-    const {name, thumbnail, description, tags} = data.categories.filter((item) => item.category_name === category)[0];
-    console.log(videos);
+    const {name, thumbnail, description, tags} = categoryDetails[0];
 
     useEffect(() => {
-        (async function() {
-            
-            try{
-            const response = await axios.get(`https://apirestream.sohamsshah.repl.co/categories/${category}`) ;
-            if(response.status === 200) {
-              setVideo(response.data.videos)}
-             }
-             catch(error){
-                 
-                 console.log(error.message)
-             }
-          })()
+        (async function () {
+            try {
+                const response = await axios.get(`https://apirestream.sohamsshah.repl.co/categories/${category}`);
+                if (response.status === 200) {
+                    
+                    setCategoryVideos(response.data.videos)
+                    setCategoryDetails(response.data.category)
+                }
+            }
+            catch (error) {
+
+                console.log(error.message)
+            }
+        })()
     }, [])
     return (
-        <div>        
-        <CategoryDetails name={name} thumbnail={thumbnail} description={description} tags={tags}/>
         <div>
-        <Typography className="category__subheading" fontSize="xl" fontWeight="semibold">Videos</Typography>
-        <VideoGroup>
-            {categoryVideos.map(({name, creator_id, thumbnail, id, category}) => <Video category = {category} name={name} thumbnail={thumbnail} redirect={`/watch/${id}`} creator_details={searchCreator(data, creator_id)} />)}
-        </VideoGroup>
-        </div>
+            {
+                (categoryDetails !== "Loading...") ? <CategoryDetails name={name} thumbnail={thumbnail} description={description} tags={tags} /> :categoryDetails
+            }
+            
+            <div>
+                <Typography className="category__subheading" fontSize="xl" fontWeight="semibold">Videos</Typography>
+
+
+                <VideoGroup>
+                    {categoryVideos !== "Loading..." ? categoryVideos.map(({ name, creator_id, thumbnail, _id, category_name }) => <Video category={category_name} name={name} thumbnail={thumbnail} redirect={`/watch/${_id}`} creator_details={searchCreator(data, creator_id)} />) : categoryVideos
+                    }
+                </VideoGroup>
+            </div>
         </div>
 
     )
